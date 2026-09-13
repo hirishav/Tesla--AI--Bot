c9e59b123c1b29fc98b798f1e5b702033be1bc97
Rishav <das.rishavdas2005@gmail.com>
2026-09-13 19:09:13 +0530

    Switch yt-dlp default search to SoundCloud to bypass YouTube bot blocks on Render

--- cogs/music.py
+++ cogs/music.py
@@ -20,7 +20,7 @@ ytdl_format_options = {
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
-    'default_search': 'auto',
+    'default_search': 'scsearch',
     'source_address': '0.0.0.0',
 }
 
